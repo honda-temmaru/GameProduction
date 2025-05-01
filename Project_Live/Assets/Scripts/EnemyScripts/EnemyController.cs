3ae0f6c6c857eb91148b12ea378b401c28905eab
lastre1208 <lastobserver.1208@gmsail.com>
2025-05-01 13:52:28 +0900

    不具合修正＆敵がプレイヤーの方向を向くようにしました。

--- Project_Live/Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Project_Live/Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -8,11 +8,12 @@ public class EnemyController : MonoBehaviour
     public float detectionRange = 10f;
     public float attackRange = 2f;
     public float moveSpeed = 3f;
+    public float rotateSpeed;
     private Transform player;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("LookPoint").transform;
     }
 
     // Update is called once per frame
@@ -25,16 +26,29 @@ public class EnemyController : MonoBehaviour
         }
         else if (distance <= detectionRange)
         {
-            MoveTowardsPlayer();
+            MoveTowardsPlayer();  
+           
         }
+      
     }
     void MoveTowardsPlayer()
     {
         Vector3 direction = (player.position - transform.position).normalized;
+        
         transform.position += direction * moveSpeed * Time.deltaTime;
+        LookPlayer();
     }
     void Attack()
     {
         Debug.Log("UŒ‚III");
+    }
+    void LookPlayer()//YŽ²‚¾‚¯•Ï‚¦‚é
+    {
+      Vector3 target=player.transform.position;
+        target.y = transform.position.y;
+       Vector3 direction=(target-transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
     }
 }
