b9a60e0a0c4a2cd95fb51a26ad3713994be125df
lastre1208 <lastobserver.1208@gmsail.com>
2025-06-09 03:14:40 +0900

    バズリショットの機能を一部実装しました
    カメラの切り替えと操作を行えるようにしました。現在は一定時間が経過したら元のカメラに戻るようにしています。
    評価システムはまだ実装していません。
    左スティックで前後左右、右スティックでカメラの向き変更、LRで高度調整ができます。

--- Project_Live/Assets/Scripts/PlayerScripts/PlayerActionScripts/BazuriShot.cs
+++ Project_Live/Assets/Scripts/PlayerScripts/PlayerActionScripts/BazuriShot.cs
@@ -1,11 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BazuriShot : MonoBehaviour
+using UnityEngine.InputSystem;
+//作成者:福島
+public class BazuriShot : MonoBehaviour// バズリショットモードの切り替えの管理
 {
+    [SerializeField] PlayerInput playerInput;
+    [Header("メインのカメラ")]
+    [SerializeField] GameObject mainCamera;
+    [Header("バズリショットの際に操作するカメラ")]
+    [SerializeField] GameObject bazuriCamera;
+    public GameObject BazuriCamera
+    {
+        get
+        {   return bazuriCamera;
+        }
+        set
+        {    bazuriCamera = value;
+        }
+    }
+    [Header("カメラの操作時間")]
+    [SerializeField] float cameraTime;
+    [Header("スロー時のゲーム速度(1未満じゃないとスローにならない)")]
+    [SerializeField] float slowSpeed;
+
+    [SerializeField] Transform defaultPos;
+    private bool isBazuri = false;
+    public bool IsBazuri
+    {
+        get { return isBazuri; }
+    }
+    private float  count;
+    
+    private void Start()
+    {
+        bazuriCamera.SetActive(false);
+        defaultPos=bazuriCamera.transform;
+    }
+
+    private void Update()
+    {
+        if (isBazuri)
+        {
+            count += Time.unscaledDeltaTime;
+            if (count >= cameraTime)
+            {
+                isBazuri = false;
+                mainCamera.SetActive(true);
+                bazuriCamera.SetActive(false);
+                Time.timeScale = 1;
+                playerInput.SwitchCurrentActionMap("Player");
+                bazuriCamera.transform.position = defaultPos.position;
+                count = 0;
+
+            }
+        }
+    }
     public void TryBazuriShot()
     {
+        if (isBazuri) return;
         Debug.Log("バズりショット発動！");
+        isBazuri = true;
+        mainCamera.SetActive(false);
+        bazuriCamera.SetActive(true);
+        Time.timeScale = slowSpeed;
+        playerInput.SwitchCurrentActionMap("Bazuri");
+
     }
 }
